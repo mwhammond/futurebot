8dc2cd8f4d61cf0b32f55a7ba1e926e2b96a6ad0
mwhammond <m.w.hammond@live.co.uk>
2013-05-13 22:40:35 +0100

    fixed links and minor update to UI
    links were pointing to search instead of tag

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,8 +6,10 @@ class PostsController < ApplicationController
 
    if params[:search]
     @posts = Post.find(:all, :conditions => ['contentSummary LIKE ? OR tags LIKE ?', "%#{params[:search]}%", "%#{params[:search]}%"])
+   
    elsif params[:topics]
-    @posts = Post.find(:all, :conditions => ['tags LIKE ?', "%#{params[:search]}%"])
+    @posts = Post.find(:all, :conditions => ['tags LIKE ?', "%#{params[:topics]}%"])
+   
    elsif params[:view]
      case params[:view]
       when 'top'
@@ -96,18 +98,9 @@ class PostsController < ApplicationController
   # PUT /posts/1.json
   def update
     @post = Post.find(params[:id])
-    if params[:vote]
-        case params[:vote]
-        when 'junk'
-          @post.update_attribute(:score, 0)
-        when 'up'
-          @post.increment!(:score)
-        when 'down'
-          @post.decrement!(:score)
-        end
-      end
+
           respond_to do |format|
-     #  @post.update_attributes(params[:post])
+       @post.update_attributes(params[:post])
         format.html { redirect_to posts_url, notice: 'Post was successfully updated.' }
         format.json { head :no_content }
       end   
