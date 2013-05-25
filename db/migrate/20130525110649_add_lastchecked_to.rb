5e86771340325124d15684b0ec9e65227530c715
mwhammond <m.w.hammond@live.co.uk>
2013-05-25 14:26:50 +0100

    feeds check last updated_at

---
+++ db/migrate/20130525110649_add_lastchecked_to.rb
@@ -0,0 +1,7 @@
+class AddLastcheckedTo < ActiveRecord::Migration
+  def up
+  end
+
+  def down
+  end
+end
--- lib/simple.rb
+++ lib/simple.rb
@@ -1,43 +1,33 @@
-#class Simple <ActiveRecord::Base
-
 CharacterFollow = 30;
-
-
-
 
 	### get latest feeds and store in ##############
 
 	def update_from_feed()
-		# ADD A LAST CHECKED TIME FIELD!
+		
+	@links = Link.all 
 
-	@links = Link.all # change to select only new ones in the controller
+	@links.each do |link| 
 
- 	@links.each do |link| 
-
-	feed = Feedzirra::Feed.fetch_and_parse(link.url)
-
- 	
+		feed = Feedzirra::Feed.fetch_and_parse(link.url)
 
 		puts "pulling feeds"
-		puts link.url
-		# add something to handle if it doesn't work
-		# CHECK FOR AND REMOVE SPECIAL CHARACTERS
-
-		# feed.last_modified - if it's been modified since last time
 		feed.entries.each do |entry|
-			# if the post occured after it was last checked
-			# check that it's a decent length
-			if entry.url =~ /^#{URI::regexp}$/
 
-			find_keywords(entry.url, link.tags)
-			puts entry.url	
-			else
+			if entry.published > link.updated_at
+
+				if entry.url =~ /^#{URI::regexp}$/
+					find_keywords(entry.url, link.tags)
+					puts entry.url	
+				else
 					puts "bad url"
 					puts entry.url	
+				end
+					
 			end
-			end
-
 		end
+
+	end
+
 end
 
 
@@ -53,7 +43,7 @@ def find_keywords(feedEntryUrl, tags)
 	#paraString = para.to_s	
 	paraString = para.text
 
- 		@keywords.each do |keyword| 
+	@keywords.each do |keyword| 
 
 		# use regex to find the keywrods and end of sentences - probably could be more effecient!	
 		# CHANGE TO DYNAMIC KEYWORD! 
@@ -63,12 +53,12 @@ def find_keywords(feedEntryUrl, tags)
 		dots = paraString.enum_for(:scan, /[\.\?\!]/).map { Regexp.last_match.begin(0) }
 
 		# find the end of a sentence immediatly before a keyword
-	
+
 		if dots.length >=1 # maybe check that it's not a title by having a few fullstops in a P
 
 			positions.each do |position|
 
-			dotBefore = dots.find_all {|i| i< position}.max
+				dotBefore = dots.find_all {|i| i< position}.max
 			dotBefore=-2 if dotBefore.nil? # start of string if no punctuation before
 			# if it's the last sentence we might actually want to go back one
 
@@ -76,15 +66,17 @@ def find_keywords(feedEntryUrl, tags)
 
 			puts paraString
 			Post.create!(:title => "bam",:content => paraString,:contentSummary => result,:score => 1, :image => "radio.jpg",:link => feedEntryUrl, :tags => tags, :datefound => DateTime.now, :keyword => keyword.word) 
-	
+
 	end # each keyword
 
 	end #each
 
 	end #if a few sentences
 
-		
-	end
+
+end
+
+Link.update_all(:updated_at => Time.now)
 
 end
 
